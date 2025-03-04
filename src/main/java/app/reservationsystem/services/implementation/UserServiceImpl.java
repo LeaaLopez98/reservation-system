fa01e5d5db57910f141266ba4218a2742b211224
lea <lea@lea.lea.com>
2025-03-04 16:03:47 -0300

    Add implementation of login method in UserService

--- src/main/java/app/reservationsystem/services/implementation/UserServiceImpl.java
+++ src/main/java/app/reservationsystem/services/implementation/UserServiceImpl.java
@@ -3,11 +3,16 @@ package app.reservationsystem.services.implementation;
 import app.reservationsystem.persistence.entity.Owner;
 import app.reservationsystem.persistence.entity.Player;
 import app.reservationsystem.persistence.entity.Role;
+import app.reservationsystem.persistence.entity.UserAccount;
 import app.reservationsystem.persistence.repository.OwnerRepository;
 import app.reservationsystem.persistence.repository.PlayerRepository;
+import app.reservationsystem.persistence.repository.UserRepository;
 import app.reservationsystem.presentation.dto.*;
+import app.reservationsystem.services.interfaces.JwtService;
 import app.reservationsystem.services.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +22,29 @@ public class UserServiceImpl implements UserService {
 
     private final PasswordEncoder encoder;
 
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final UserRepository userRepository;
     private final PlayerRepository playerRepository;
     private final OwnerRepository ownerRepository;
 
     @Override
     public LoginResponse login(LoginRequest request) {
-        return null;
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+            );
+
+            UserAccount user = userRepository.findByUsername(request.getUsername()).get();
+
+            String token = jwtService.generateToken(user);
+
+            return new LoginResponse(token);
+        } catch (Exception e) {
+            throw new RuntimeException("Username or password incorrect");
+        }
     }
 
     @Override
