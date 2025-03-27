b6e48460bf1aa4c7f81450bc5ff337ad67b36399
lea <lea@lea.lea.com>
2025-03-27 14:29:11 -0300

    Add JWT authentication filter

---
+++ src/main/java/app/reservationsystem/config/JwtFilter.java
@@ -0,0 +1,74 @@
+package app.reservationsystem.config;
+
+import app.reservationsystem.services.JwtService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException
+    {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String jwtToken = authHeader.substring(7);
+            String username = jwtService.extractUsername(jwtToken);
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (username != null && authentication == null) {
+
+                Claims claims = jwtService.getAllClaims(jwtToken);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
+
+                if (jwtService.isValidToken(jwtToken, claims.getSubject())) {
+                    var authToken = new UsernamePasswordAuthenticationToken(
+                            claims.getSubject(),
+                            null,
+                            userDetails.getAuthorities()
+                    );
+
+                    authToken.setDetails(claims);
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+            SecurityContextHolder.clearContext();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
