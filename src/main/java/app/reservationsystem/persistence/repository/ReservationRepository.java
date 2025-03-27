ddcce1be66c929de0d75284bbcaa5165d3ef2c53
lea <lea@lea.lea.com>
2025-03-27 15:18:15 -0300

    Update method to check if a reservation exists that can overlap, adding a constraint for the reservation status

--- src/main/java/app/reservationsystem/persistence/repository/ReservationRepository.java
+++ src/main/java/app/reservationsystem/persistence/repository/ReservationRepository.java
@@ -1,7 +1,9 @@
 package app.reservationsystem.persistence.repository;
 
 import app.reservationsystem.persistence.entity.Reservation;
+import app.reservationsystem.persistence.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +13,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findAllByPlayerIdUser(Long idPlayer);
     List<Reservation> findAllByFieldClubOwnerIdUser(Long idUser);
 
-    boolean existsByFieldIdFieldAndDateBeginBeforeAndDateEndAfter(
+    boolean existsByStatusNotLikeAndFieldIdFieldAndDateBeginBeforeAndDateEndAfter(
+            Status status,
             Integer idField,
             LocalDateTime dateEnd,
             LocalDateTime dateBegin
