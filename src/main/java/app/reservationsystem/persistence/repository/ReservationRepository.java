b61688e6c5626f3e679350606e1ffce03726ec61
lea <lea@lea.lea.com>
2025-03-27 14:40:09 -0300

    Fix query to ensure a reservation does not overlap with another

--- src/main/java/app/reservationsystem/persistence/repository/ReservationRepository.java
+++ src/main/java/app/reservationsystem/persistence/repository/ReservationRepository.java
@@ -2,7 +2,6 @@ package app.reservationsystem.persistence.repository;
 
 import app.reservationsystem.persistence.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,19 +9,7 @@ import java.util.List;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findAllByPlayerIdUser(Long idPlayer);
-    List<Reservation> findAllByFieldIdField(Integer idField);
-
-    @Query(
-            value = """
-            select r.*
-            from reservation r
-            join field f on r.id_field = f.id_field
-            join club c on f.id_club = c.id_club
-            where c.id_club = :idClub
-            """,
-            nativeQuery = true
-    )
-    List<Reservation> findAllByClub(Integer idClub);
+    List<Reservation> findAllByFieldClubOwnerIdUser(Long idUser);
 
     boolean existsByFieldIdFieldAndDateBeginBeforeAndDateEndAfter(
             Integer idField,
