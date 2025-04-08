cb98ccfa657881073fe3cb0468724c5914eff1d4
lea <lea@lea.lea.com>
2025-04-08 15:58:34 -0300

    Remove NotBlank annotation in ReservationRequestDTO

--- src/main/java/app/reservationsystem/reservations/dto/ReservationRequestDTO.java
+++ src/main/java/app/reservationsystem/reservations/dto/ReservationRequestDTO.java
@@ -2,7 +2,6 @@ package app.reservationsystem.reservations.dto;
 
 
 import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,17 +14,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ReservationRequestDTO {
 
-    @NotBlank(message = "Field id is required")
     @NotNull(message = "Field id is required")
     private Integer idField;
 
     @NotNull(message = "Date begin is required")
-    @NotBlank(message = "Date begin is required")
     @Future(message = "Date begin must be in the future")
     private LocalDateTime dateBegin;
 
     @NotNull(message = "Date end is required")
-    @NotBlank(message = "Date end is required")
     @Future(message = "Date end must be in the future")
     private LocalDateTime dateEnd;
 
