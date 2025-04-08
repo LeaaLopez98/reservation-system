f32ce62ae3966c38a05a1024fc78af2731ed99cc
lea <lea@lea.lea.com>
2025-04-08 10:57:14 -0300

    Add MapStruct methods to update Field and Club entity from DTO

--- src/main/java/app/reservationsystem/clubs/mapper/ClubMapper.java
+++ src/main/java/app/reservationsystem/clubs/mapper/ClubMapper.java
@@ -1,15 +1,23 @@
 package app.reservationsystem.clubs.mapper;
 
+import app.reservationsystem.clubs.dto.ClubUpdateDTO;
 import app.reservationsystem.clubs.entity.Club;
 import app.reservationsystem.clubs.dto.ClubRequestDTO;
 import app.reservationsystem.clubs.dto.ClubResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", uses = FieldMapper.class)
+@Mapper(
+        componentModel = "spring",
+        uses = FieldMapper.class,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ClubMapper {
 
 
     Club DtoToEntity(ClubRequestDTO clubDTO);
     ClubResponseDTO entityToDto(Club club);
+
+    void updateClubFromDto(ClubUpdateDTO dto, @MappingTarget Club club);
 
 }
--- src/main/java/app/reservationsystem/clubs/mapper/FieldMapper.java
+++ src/main/java/app/reservationsystem/clubs/mapper/FieldMapper.java
@@ -1,17 +1,22 @@
 package app.reservationsystem.clubs.mapper;
 
+import app.reservationsystem.clubs.dto.FieldUpdateDTO;
 import app.reservationsystem.clubs.entity.Field;
 import app.reservationsystem.clubs.dto.FieldRequestDTO;
 import app.reservationsystem.clubs.dto.FieldResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface FieldMapper {
 
     @Mapping(source = "club.idClub", target = "idClub")
     @Mapping(source = "club.address", target = "address")
     FieldResponseDTO entityToDto(Field field);
     Field dtoToEntity(FieldRequestDTO fieldDTO);
+
+    void updateFieldFromDto(FieldUpdateDTO dto, @MappingTarget Field field);
 
 }
