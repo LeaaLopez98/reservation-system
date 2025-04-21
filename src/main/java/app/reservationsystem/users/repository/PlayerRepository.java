c67029f7faf636650862f7ca3e37a4e67f7fc9ef
lea <lea@lea.lea.com>
2025-04-21 13:48:47 -0300

    Move existsByUsername and existsByEmail methods from the Player and Owner repositories to the UserRepository to reduce code duplication

--- src/main/java/app/reservationsystem/users/repository/PlayerRepository.java
+++ src/main/java/app/reservationsystem/users/repository/PlayerRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
-
 }
